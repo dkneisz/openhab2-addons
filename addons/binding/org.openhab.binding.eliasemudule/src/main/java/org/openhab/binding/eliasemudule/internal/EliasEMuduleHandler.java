2748e821897f6f5b58648dac1c7e29c24fe1c8f1
Dave <d.kneisz@gmail.com>
2018-09-18 22:16:43 +0200

    - fixing build-errors

--- addons/binding/org.openhab.binding.eliasemudule/src/main/java/org/openhab/binding/eliasemudule/internal/EliasEMuduleHandler.java
+++ addons/binding/org.openhab.binding.eliasemudule/src/main/java/org/openhab/binding/eliasemudule/internal/EliasEMuduleHandler.java
@@ -1,22 +1,15 @@
 /**
- * Copyright (c) 2014,2018 by the respective copyright holders.
+ * Copyright (c) 2010-2018 by the respective copyright holders.
  *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.openhab.binding.eliasemudule.internal;
 
-import static org.openhab.binding.eliasemudule.internal.EliasEMuduleBindingConstants.*;
-
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.eliasemudule.internal.EliasEMuduleConfiguration;
 import org.eclipse.smarthome.core.thing.ChannelUID;
 import org.eclipse.smarthome.core.thing.Thing;
 import org.eclipse.smarthome.core.thing.ThingStatus;
@@ -46,17 +39,12 @@ public class EliasEMuduleHandler extends BaseThingHandler {
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        if (CHANNEL_1.equals(channelUID.getId())) {
-            if (command instanceof RefreshType) {
-                // TODO: handle data refresh
-            }
-            
-            // TODO: handle command
-
-            // Note: if communication with thing fails for some reason,
-            // indicate that by setting the status with detail information:
-            // updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR,
-            // "Could not control device at IP address x.x.x.x");
+        switch (channelUID.getId()) {
+            case CHANNEL_MODULES:
+                if (command instanceof RefreshType) {
+                    // TODO: handle data refresh
+                }
+                break;
         }
     }
 
@@ -66,8 +54,8 @@ public class EliasEMuduleHandler extends BaseThingHandler {
         config = getConfigAs(EliasEMuduleConfiguration.class);
 
         // TODO: Initialize the handler.
-        // The framework requires you to return from this method quickly. Also, before leaving this method a thing 
-        // status from one of ONLINE, OFFLINE or UNKNOWN must be set. This might already be the real thing status in 
+        // The framework requires you to return from this method quickly. Also, before leaving this method a thing
+        // status from one of ONLINE, OFFLINE or UNKNOWN must be set. This might already be the real thing status in
         // case you can decide it directly.
         // In case you can not decide the thing status directly (e.g. for long running connection handshake using WAN
         // access or similar) you should set status UNKNOWN here and then decide the real status asynchronously in the
@@ -77,7 +65,7 @@ public class EliasEMuduleHandler extends BaseThingHandler {
         // the framework is then able to reuse the resources from the thing handler initialization.
         // we set this upfront to reliably check status updates in unit tests.
         updateStatus(ThingStatus.UNKNOWN);
-        
+
         // Example for background initialization:
         scheduler.execute(() -> {
             boolean thingReachable = true; // <background task with long running initialization here>
